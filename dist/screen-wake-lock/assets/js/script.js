10c20b94d24091db4c98a3f220b5325adc5096fb
Rowdy Rabouw <hi@rowdy.codes>
2025-05-24 14:18:43 +0200

    feat: update screen-wake-lock and web-share demos

--- dist/screen-wake-lock/assets/js/script.js
+++ dist/screen-wake-lock/assets/js/script.js
@@ -1,17 +1,26 @@
+const wakeLockRequest = document.querySelector("#request-wakelock");
+const wakeLockRelease = document.querySelector("#release-wakelock");
+const wakelockStatus = document.querySelector("#wakelock-status");
+
 let wakelock = null;
 
 const requestWakelock = async () => {
   wakelock = await navigator.wakeLock.request("screen");
   console.log("Wakelock is active");
-  alert("Wakelock is active");
+  wakeLockRequest.classList.add("hidden");
+  wakeLockRelease.classList.remove("hidden");
+  wakelockStatus.textContent = "Wakelock is active";
 };
 
 const releaseWakelock = async () => {
   if (wakelock !== null) {
     await wakelock.release();
     console.log("Wakelock is released");
+    wakeLockRequest.classList.remove("hidden");
+    wakeLockRelease.classList.add("hidden");
+    wakelockStatus.textContent = "Wakelock is released";
   }
 };
 
-document.getElementById("request-wakelock").addEventListener("click", requestWakelock);
-document.getElementById("release-wakelock").addEventListener("click", releaseWakelock);
+wakeLockRequest.addEventListener("click", requestWakelock);
+wakeLockRelease.addEventListener("click", releaseWakelock);
